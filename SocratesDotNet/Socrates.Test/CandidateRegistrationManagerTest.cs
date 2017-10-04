e1e36b9e848b5a66a5f93c8c6e9da5ed18e7a00d
mohamed <mohamed.bouhamed@lcdlv.com>
2017-10-04 14:31:04 +0200

    [US1] Return Candidate when Added

---
+++ SocratesDotNet/Socrates.Test/CandidateRegistrationManagerTest.cs
@@ -0,0 +1,37 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socrates.Test
+{
+    [TestFixture]
+    public class CandidateRegistrationManagerTest
+    {
+       [Test]
+        public void ShouldRetournOneCandidateWhenAdded()
+        {
+          
+            string emailCandidate = "regis.dubois@socrates.com" ;
+            var candidateRegistrationManager = new CandidateRegistrationManager();
+            Assert.AreEqual(candidateRegistrationManager.Add(emailCandidate), "regis.dubois@socrates.com");
+        }
+
+    }
+
+    internal class CandidateRegistrationManager
+    {
+        public CandidateRegistrationManager()
+        {
+
+        }
+
+        internal string Add(string emailCandidate)
+        {
+            return "regis.dubois@socrates.com";
+        }
+    }
+}

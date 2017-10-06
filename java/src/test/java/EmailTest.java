c43308511bc5a0c0d37d9822fd6628e51bc344b0
sabinef <sabine.ferreira@lcdlv.fr>
2017-10-06 12:13:15 +0200

    [US1]: Rename email tests

--- java/src/test/java/EmailTest.java
+++ java/src/test/java/EmailTest.java
@@ -3,17 +3,17 @@ import org.junit.Test;
 public class EmailTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void emptyEmailThrowException() throws Exception {
+    public void should_thrown_exception_when_creating_an_empty_email() throws Exception {
         Email.of("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void nullEmailThrowException() throws Exception {
+    public void should_thrown_exception_when_creating_a_null_email() throws Exception {
         Email.of(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void notEmailValidThrowException() throws Exception {
+    public void should_thrown_exception_when_creating_a_not_valid_email() throws Exception {
         Email.of("emailNotValid.ok");
     }
 }

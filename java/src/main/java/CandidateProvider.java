6e5ade2bd10ba212537a96197985f4e06fff32f6
MelodyBibi <melody.scholtes@gmail.com>
2017-10-10 11:47:40 +0200

    [US1] : Add one candidate to manager and verify if exist

--- java/src/main/java/CandidateProvider.java
+++ java/src/main/java/CandidateProvider.java
@@ -1,7 +1,22 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Collection;
+import java.util.HashSet;
 
 public class CandidateProvider {
 
+    private Collection<Email> emails = new HashSet<>();
+
     public boolean isEmpty() {
         return true;
+    }
+
+    public boolean exist(Email email) {
+        return emails.contains(email);
+    }
+
+
+    public void add(Email email) {
+        emails.add(email);
     }
 }
--- java/src/test/java/CandidateProviderTest.java
+++ java/src/test/java/CandidateProviderTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CandidateProviderTest {
+    public static final String TOTO_EMAIL = "totoTahLesOuf@lcdlv.fr";
     private CandidateProvider candidateProvider;
 
     @Before
@@ -13,5 +14,13 @@ public class CandidateProviderTest {
     @Test
     public void should_have_no_email_at_initialization() {
         Assertions.assertThat(candidateProvider.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void should_adding_a_candidate() throws Exception {
+        Email email = Email.of(TOTO_EMAIL);
+        candidateProvider.add(email);
+        Assertions.assertThat(candidateProvider.exist(email)).isTrue();
+
     }
 }

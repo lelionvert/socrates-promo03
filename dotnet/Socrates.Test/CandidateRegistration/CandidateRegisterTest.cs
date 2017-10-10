d81fd20e7b97d3fa14eb094abdb9e44ab23ec937
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-10 18:01:30 +0200

    [US1] - Register should call HasAlready from MockCandidateProvider

--- dotnet/Socrates.Test/CandidateRegistration/CandidateRegisterTest.cs
+++ dotnet/Socrates.Test/CandidateRegistration/CandidateRegisterTest.cs
@@ -12,95 +12,17 @@ namespace Socrates.Test.Services
         private const string JulieMarechalEmail = "julie.marechal@microsoft.com";
 
         [Test]
-        public void GetCandidates_Should_Return_No_Candidates_When_Initializing_Register()
+        public void Register_Should_Call_HasAlready()
         {
-
-            var candidateProvider = new CandidateProvider();
+            var candidateProvider = new MockCandidateProvider();
             var candidateRegister = new CandidateRegister(candidateProvider);
-
-            Check.That(candidateProvider.IsEmpty()).IsTrue();
+            
+            candidateRegister.Register(new Candidate(Email.Of(RegisDuboisEmail)));
+            Check.That(candidateProvider.hasAlreadyWasCalled).IsTrue();
         }
 
-        [Test]
-        public void GetCandidates_Should_Return_One_Candidate_When_Already_One_Exists()
-        {
-            var regisDuboisCandidate = new Candidate(Email.Of(RegisDuboisEmail));
-            var candidateProvider = new CandidateProvider(regisDuboisCandidate);
 
-            var candidateRegister = new CandidateRegister(candidateProvider);
-
-            var expectedCandidate = new Candidate(Email.Of(RegisDuboisEmail));
-
-            Check.That(candidateProvider.HasAlready(expectedCandidate)).IsTrue();
-        }
-
-        [TestCase(RegisDuboisEmail)]
-        [TestCase(FannyDuboisEmail)]
-        public void Register_Should_Return_The_Candidate_Previously_Added(string candidateEmail)
-        {
-            var candidate = new Candidate(Email.Of(candidateEmail));
-            var candidateProvider = new CandidateProvider();
-            var candidateRegistrer = new CandidateRegister(candidateProvider);
-
-            candidateRegistrer.Register(candidate);
-
-            Check.That(candidateProvider.HasAlready(new Candidate(Email.Of(candidateEmail)))).IsTrue();
-        }
-
-        [Test]
-        public void Register_Should_Insert_Multiples_Candidates_And_Verify_Their_Existance()
-        {
-            var candidateProvider = new CandidateProvider();
-            var candidateRegister = new CandidateRegister(candidateProvider);
-
-           candidateRegister.Register(new Candidate(Email.Of(FannyDuboisEmail)));
-           candidateRegister.Register(new Candidate(Email.Of(RegisDuboisEmail)));
-
-            Check.That(candidateProvider.HasAlready(new Candidate(Email.Of(RegisDuboisEmail)), new Candidate(Email.Of(FannyDuboisEmail)))).IsTrue();
-        }
-
-        [TestCase(RegisDuboisEmail)]
-        [TestCase(FannyDuboisEmail)]
-        public void Add_Should_Return_Unique_Candidate_Even_If_You_Try_To_Add_Existing_Candidate(string candidateEmail)
-        {
-            var candidateProvider = new CandidateProvider();
-
-            var candidateRegister = new CandidateRegister(candidateProvider);
-
-            candidateRegister.Register(new Candidate(Email.Of(candidateEmail)));
-
-            candidateRegister.Register(new Candidate(Email.Of(candidateEmail)));
-
-            Check.That(candidateProvider.GetCandidates()).Contains(new Candidate(Email.Of(candidateEmail))).Only().Once();
-        }
-
-        [Test]
-        public void Add_Should_Add_One_Candidate_When_Multiples_Candidate_Exists()
-        {
-            var candidateProvider = new CandidateProvider(new Candidate(Email.Of(RegisDuboisEmail)), new Candidate(Email.Of(FannyDuboisEmail)));
-
-            var candidateRegister = new CandidateRegister(candidateProvider);
-
-            candidateRegister.Register(new Candidate(Email.Of(JulieFournierEmail)));
-
-            var candidates = candidateProvider.GetCandidates();
-
-            Check.That(candidates).Contains(new Candidate(Email.Of(RegisDuboisEmail)), new Candidate(Email.Of(FannyDuboisEmail)), new Candidate(Email.Of(JulieFournierEmail))).Only().Once();
-        }
-
-        [TestCase(JulieMarechalEmail, JulieFournierEmail)]
-        public void Add_Should_Add_Multiple_Candidate_Emails_When_Multiple_Candidate_Emails_Exist(string firstCandidateEmail, string secondCandidateEmail)
-        {
-            var candidateProvider = new CandidateProvider(new Candidate(Email.Of(RegisDuboisEmail)), new Candidate(Email.Of(FannyDuboisEmail)));
-
-            var candidateRegister = new CandidateRegister(candidateProvider);
-
-            candidateRegister.Register(new Candidate(Email.Of(firstCandidateEmail)));
-            candidateRegister.Register(new Candidate(Email.Of(secondCandidateEmail)));
-
-            var candidates = candidateProvider.GetCandidates();
-            Check.That(candidates).Contains(new Candidate(Email.Of(RegisDuboisEmail)), new Candidate(Email.Of(FannyDuboisEmail)), new Candidate(Email.Of(firstCandidateEmail)), new Candidate(Email.Of(secondCandidateEmail))).Only().Once();
-        }
+        
 
 
     }
---
+++ dotnet/Socrates.Test/CandidateRegistration/MockCandidateProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Socrates.CandidateRegistration;
+
+namespace Socrates.Test.Services
+{
+    internal class MockCandidateProvider : ICandidateProvider
+    {
+        public bool hasAlreadyWasCalled { get; private set; };
+
+        public void AddCandidate(Candidate candidate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Email> GetCandidateEmails()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasAlready(params Candidate[] candidates)
+        {
+            hasAlreadyWasCalled = true;
+
+            return true;
+        }
+    }
+}

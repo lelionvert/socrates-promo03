3bebd4fdbe34567cec09f2e3a233fbabff1afbad
MelodyBibi <melody.scholtes@gmail.com>
2017-10-11 15:15:50 +0200

    [US1] : Add show list candidates email into console

--- java/src/main/java/Application.java
+++ java/src/main/java/Application.java
@@ -1,10 +1,19 @@
+import com.lacombe.promo3.registration.CandidateRegistrationManager;
+import com.lacombe.promo3.registration.model.Email;
+import com.lacombe.promo3.registration.repository.DefaultCandidateRepository;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
+    private static CandidateRegistrationManager candidateRegistrationManager;
+
+    public static void main(String[] args) throws IOException {
+        initManager();
+
         Scanner scanner = new Scanner(System.in);
         int choice;
         do {
@@ -21,20 +30,31 @@ public class Application {
             choice = scanner.nextInt();
 
             switch (choice) {
-                case 1 : getCandidatesEmail(); break;
+                case 1 : showCandidatesEmail(); break;
                 case 2 : addCandidate(); break;
             }
 
+            System.in.read();
         } while (choice != 0);
 
         System.exit(0);
+    }
+
+    private static void initManager() {
+        DefaultCandidateRepository defaultCandidateRepository = new DefaultCandidateRepository();
+        candidateRegistrationManager = new CandidateRegistrationManager(defaultCandidateRepository);
     }
 
     private static void addCandidate() {
         throw new NotImplementedException();
     }
 
-    private static void getCandidatesEmail() {
-        throw new NotImplementedException();
+    private static void showCandidatesEmail() {
+        Collection<Email> emails = candidateRegistrationManager.findEmails();
+        if(emails.isEmpty()) {
+            System.out.println("Aucun candidat trouvé.");
+        } else {
+            emails.forEach(System.out::println);
+        }
     }
 }

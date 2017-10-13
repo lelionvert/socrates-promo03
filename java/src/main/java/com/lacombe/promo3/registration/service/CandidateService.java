4a4d941b82fffae8bd2a7b770795abd83b1eaa3f
valmey <valmey@lcdlv.fr>
2017-10-13 12:01:13 +0200

    Add APIOPERATION ANNOTATIONS

--- java/src/main/java/com/lacombe/promo3/registration/service/CandidateService.java
+++ java/src/main/java/com/lacombe/promo3/registration/service/CandidateService.java
@@ -1,7 +1,10 @@
 package com.lacombe.promo3.registration.service;
 
+import com.lacombe.promo3.registration.CandidateRegistrationManager;
 import com.lacombe.promo3.registration.model.Candidate;
 import com.lacombe.promo3.registration.model.Email;
+import com.lacombe.promo3.registration.repository.DefaultCandidateRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -13,11 +16,17 @@ import java.util.Optional;
 @Service
 public class CandidateService {
 
+    private final Email SABINE_EMAIL = Email.of("sabine@lcdlv.fr");
+    private final Email CYRIL_EMAIL = Email.of("cyril@lcdlv.fr");
+    private final Candidate SABINE_CANDIDATE = new Candidate(SABINE_EMAIL);
+    private final Candidate CYRIL_CANDIDATE = new Candidate(CYRIL_EMAIL);
+
+    private final DefaultCandidateRepository defaultCandidateRepository = DefaultCandidateRepository.withExisting(SABINE_CANDIDATE, CYRIL_CANDIDATE);
+    private final CandidateRegistrationManager candidateRegistrationManager = new CandidateRegistrationManager(defaultCandidateRepository);
+
+    @ApiOperation(value = "Get a candidates", response = Candidate.class)
     public Optional<Candidate> getItem(String email) {
-        Email SABINE_EMAIL = Email.of("sabine@lcdlv.fr");
-        Email CYRIL_EMAIL = Email.of("cyril@lcdlv.fr");
-        Candidate SABINE_CANDIDATE = new Candidate(SABINE_EMAIL);
-        Candidate CYRIL_CANDIDATE = new Candidate(CYRIL_EMAIL);
+
         List<Candidate> candidates = Arrays.asList(SABINE_CANDIDATE, CYRIL_CANDIDATE);
         for (Candidate candidate : candidates) {
             if(candidate.getEmail().equals(Email.of(email))) {
@@ -27,6 +36,7 @@ public class CandidateService {
         return Optional.empty();
     }
 
+    @ApiOperation(value = "Get All Candidates", response = Collection.class)
     public Collection<Candidate> getCandidates() {
         Email SABINE_EMAIL = Email.of("sabine@lcdlv.fr");
         Email CYRIL_EMAIL = Email.of("cyril@lcdlv.fr");
@@ -34,4 +44,6 @@ public class CandidateService {
         Candidate CYRIL_CANDIDATE = new Candidate(CYRIL_EMAIL);
         return Arrays.asList(SABINE_CANDIDATE, CYRIL_CANDIDATE);
     }
+
+    //TODO add
 }

1388f97df4877271b2a686cd50ab2545a08d0d03
sabinef <sabine.ferreira@lcdlv.fr>
2017-10-13 10:04:58 +0200

    [US4]: acceptance test send mail to all

---
+++ java/src/main/java/com/lacombe/promo3/registration/EmailService.java
@@ -0,0 +1,14 @@
+package com.lacombe.promo3.registration;
+
+import com.lacombe.promo3.registration.repository.CandidateRepository;
+
+public class EmailService {
+
+    public EmailService(CandidateRepository candidateRepository, Logger logger) {
+
+    }
+
+    public void sendAskingConfirmationOrPayment() {
+
+    }
+}
---
+++ java/src/main/java/com/lacombe/promo3/registration/Logger.java
@@ -0,0 +1,5 @@
+package com.lacombe.promo3.registration;
+
+public interface Logger {
+
+}

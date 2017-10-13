3523743b3c4a06ec72d78ab6cf837ce6e829eb38
valmey <valmey@lcdlv.fr>
2017-10-13 16:48:20 +0200

    Add Consul registration
    > Add to POM.XML
    dependencies :
    - spring-boot-starter-actuator
    - spring-cloud-starter-consul-discovery
    - spring-cloud-starter-config
    
    and
    
    <dependencyManagement>
       <dependencies>
              <dependency>
    	             <groupId>org.springframework.cloud</groupId>
    		                <artifactId>spring-cloud-dependencies</artifactId>
    				           <version>Dalston.SR2</version>
    					              <type>pom</type>
    						                 <scope>import</scope>
    								        </dependency>
    									   </dependencies>
    									   </dependencyManagement>
    
    > create main/resources/bootstrap.yml
    
    spring:
     application:
        name: my-app
         cloud:
            consul:
    	     host: localhost
    	          port: 8500
    
    > Download and launch Consul with command : ./consul agent -dev
    > Restart SpringBoot Application
    > connect to Consul Web Dashboard : http://localhost:8500

--- java/src/main/java/com/lacombe/promo3/WebApplication.java
+++ java/src/main/java/com/lacombe/promo3/WebApplication.java
@@ -1,8 +1,12 @@
 package com.lacombe.promo3;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WebApplication {
 

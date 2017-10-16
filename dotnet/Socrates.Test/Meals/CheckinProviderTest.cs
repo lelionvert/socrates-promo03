031fdacf97e37945b58452050fbb21c34a18b3aa
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-16 17:09:05 +0200

    [US3] - Add CountLateCheckin method when 1 checkin before 21h Return 0 Participant

--- dotnet/Socrates.Test/Meals/CheckinProviderTest.cs
+++ dotnet/Socrates.Test/Meals/CheckinProviderTest.cs
@@ -1,5 +1,7 @@
 ﻿using NFluent;
 using NUnit.Framework;
+using Socrates.CandidateRegistration;
+using System;
 
 namespace Socrates.Test.Meals
 {
@@ -16,6 +18,22 @@ namespace Socrates.Test.Meals
 
             // ASSERT
             Check.That(countLateCheckin).IsZero();
+        }
+
+        [Test]
+        public void CountLateCheckin_Should_Return_Zero_When_1_Checkin_Before_21()
+        {
+            // SETUP
+            var participantCheckinDate = new DateTime(2017, 10, 27, 16, 30, 00);
+            var participantCheckin = new Checkin(Email.Of("Ismail@lcdlv.fr"), participantCheckinDate);
+            var checkinProvider = new CheckinProvider(participantCheckin);
+
+            // RUN
+            var countLateCheckin = checkinProvider.CountLateCheckin();
+
+            // ASSERT
+            Check.That(countLateCheckin).IsZero();
+
         }
     }
 }

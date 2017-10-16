51441bcd9b50107390741a93c002c93e3f850c42
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-16 17:44:18 +0200

    [US3] - Count 3 cold meals when 3 participants are late

--- dotnet/Socrates.Test/Meals/ColdMealsCounterAcceptanceTest.cs
+++ dotnet/Socrates.Test/Meals/ColdMealsCounterAcceptanceTest.cs
@@ -52,5 +52,26 @@ namespace Socrates.Test.Meals
             // ASSERT
             Check.That(coldMealsNumber).IsEqualTo(1);
         }
+        [Test]
+        public void CountColdMeals_Should_Return_Three_Cold_Meal_When_Participants_Checkin_After_21h()
+        {
+            // SETUP
+            var firstParticipantCheckinDate = new DateTime(2017, 10, 27, 21, 30, 00);
+            var secondParticipantCheckinDate = new DateTime(2017, 10, 27, 22, 30, 00);
+            var thirdParticipantCheckinDate = new DateTime(2017, 10, 29, 09, 30, 00);
+
+            var firstCheckin = new Checkin(Email.Of("regis.dubois@socrates.com"), firstParticipantCheckinDate);
+            var secondCheckin = new Checkin(Email.Of("ismail.nguyen@socrates.com"), secondParticipantCheckinDate);
+            var thirdCheckin = new Checkin(Email.Of("gabriel.zaafrani@socrates.com"), thirdParticipantCheckinDate);
+
+            var checkinProvider = new CheckinProvider(firstCheckin,secondCheckin,thirdCheckin);
+            var coldMealsCounter = new ColdMealsCounter(checkinProvider);
+
+            // RUN
+            var coldMealsNumber = coldMealsCounter.CountColdMeals();
+
+            // ASSERT
+            Check.That(coldMealsNumber).IsEqualTo(3);
+        }
     }
 }

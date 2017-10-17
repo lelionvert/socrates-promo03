76b8049bb852de23115d6dfb439b67c3758e7cec
MelodyBibi <melody.scholtes@gmail.com>
2017-10-17 11:04:06 +0200

    [US4] : Add message email notion in acceptance test

--- java/src/test/java/com/lacombe/promo3/registration/acceptance/US4_SendEmailAcceptanceTest.java
+++ java/src/test/java/com/lacombe/promo3/registration/acceptance/US4_SendEmailAcceptanceTest.java
@@ -1,34 +1,38 @@
 package com.lacombe.promo3.registration.acceptance;
 
-import com.lacombe.promo3.EmailSender;
+import com.lacombe.promo3.communication.EmailMessage;
+import com.lacombe.promo3.communication.EmailSender;
+import com.lacombe.promo3.communication.EmailSenderLoggedInConsole;
 import com.lacombe.promo3.registration.CandidateConfirmationChecker;
 import com.lacombe.promo3.registration.ConfirmationSender;
-import com.lacombe.promo3.registration.EmailSenderWithLogger;
-import com.lacombe.promo3.registration.model.Candidate;
-import com.lacombe.promo3.registration.model.Email;
-import com.lacombe.promo3.registration.repository.ConfirmationRepository;
-import com.lacombe.promo3.registration.repository.CandidateRepositoryDefault;
-import com.lacombe.promo3.registration.repository.ConfirmationRepositoryDefault;
+import com.lacombe.promo3.communication.EmailSenderLogged;
+import com.lacombe.promo3.shared.model.Candidate;
+import com.lacombe.promo3.shared.model.Email;
+import com.lacombe.promo3.communication.repository.ConfirmationRepository;
+import com.lacombe.promo3.shared.repository.CandidateRepositoryInMemory;
+import com.lacombe.promo3.communication.repository.ConfirmationRepositoryInMemory;
 import org.assertj.core.api.Assertions;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class US4_SendEmailAcceptanceTest {
 
-    private static final Email LUCAS_EMAIL = Email.of("lucas@lcdlv.fr");
+    private static final String LUCAS_EMAIL_STRING = "lucas@lcdlv.fr";
+    private static final Email LUCAS_EMAIL = Email.of(LUCAS_EMAIL_STRING);
     private static final Candidate LUCAS_CANDIDATE = new Candidate(LUCAS_EMAIL);
-    private static final Email JULIE_EMAIL = Email.of("julie@lcdlv.fr");
+    private static final String JULIE_EMAIL_STRING = "julie@lcdlv.fr";
+    private static final Email JULIE_EMAIL = Email.of(JULIE_EMAIL_STRING);
     private static final Candidate JULIE_CANDIDATE = new Candidate(JULIE_EMAIL);
 
     @Ignore
     @Test
-    public void should_send_a_confirmation_or_payment_email_to_all_new_candidates() throws Exception {
+    public void should_send_a_confirmation_email_to_one_new_candidates() throws Exception {
         // GIVEN
-        CandidateRepositoryDefault defaultCandidateRepository =
-                CandidateRepositoryDefault.withExisting(LUCAS_CANDIDATE, JULIE_CANDIDATE);
-        ConfirmationRepository confirmationRepository = new ConfirmationRepositoryDefault();
+        CandidateRepositoryInMemory defaultCandidateRepository =
+                CandidateRepositoryInMemory.withExisting(LUCAS_CANDIDATE, JULIE_CANDIDATE);
+        ConfirmationRepository confirmationRepository = new ConfirmationRepositoryInMemory();
 
-        EmailSender emailSender = new EmailSenderWithLogger();
+        EmailSender emailSender = new EmailSenderLoggedInConsole();
 
         CandidateConfirmationChecker candidateConfirmationChecker = new CandidateConfirmationChecker(defaultCandidateRepository);
 
@@ -40,8 +44,14 @@ public class US4_SendEmailAcceptanceTest {
         confirmationSender.execute();
 
         // THEN
-        Assertions.assertThat(((EmailSenderWithLogger)emailSender).printLog())
-                .isEqualTo("Email sent to : " + LUCAS_CANDIDATE + "\n"
-                + "Email sent to : " + JULIE_CANDIDATE);
+        EmailMessage messageLucas = EmailMessage.of().withRecipient("houssam@lcdlv.fr")
+                                    .withSender(LUCAS_EMAIL_STRING)
+                                    .withObject("Confirmation")
+                                    .withCore("Hello, please confirm or pay.")
+                                    .build();
+        Assertions.assertThat(((EmailSenderLogged)emailSender).getMessage())
+                .isEqualTo(messageLucas);
+        Assertions.assertThat(((EmailSenderLogged)emailSender).printLog())
+                .isEqualTo("Email sent to : " + LUCAS_CANDIDATE);
     }
 }

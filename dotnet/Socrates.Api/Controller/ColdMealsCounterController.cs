d9cc4af2af3d6af3852413811291a414999b30c7
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-17 17:59:38 +0200

    [US3] - Add empty Web Api projects

---
+++ dotnet/Socrates.Api/Controller/ColdMealsCounterController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace Socrates.Api.Controller
+{
+    [RoutePrefix("api/coldMealsCounter")]
+    public class ColdMealsCounterController : ApiController
+    {
+    }
+}
---
+++ dotnet/Socrates.Web/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Socrates.Web.Startup))]
+
+namespace Socrates.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configuration et services API Web
+            var httpConfig = new HttpConfiguration();
+
+            // Itinéraires de l'API Web
+            httpConfig.MapHttpAttributeRoutes();
+
+            httpConfig.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(httpConfig);
+        }
+    }
+}

687f113dfbcdc1291efb08b3a32ead88792745be
YeahMama <wallon.xu@lcdlv.fr>
2017-10-17 11:02:28 +0200

    [US3]: Should not be late when check in date is before the begin of cold meals

--- java/src/test/java/com/lacombe/promo3/CheckInTest.java
+++ java/src/test/java/com/lacombe/promo3/CheckInTest.java
@@ -25,15 +25,22 @@ public class CheckInTest {
 
     @Test
     public void should_not_be_late_when_check_in_date_is_after_thursday_night() throws Exception {
-        regisCheckIn.setCheckingDate(LocalDateTime.of(2017, Month.OCTOBER,28,0,0,0,0));
+        regisCheckIn.setCheckingDate(LocalDateTime.of(2017, Month.OCTOBER, 28, 0, 0, 0, 0));
         Boolean isLate = regisCheckIn.isLate();
         Assertions.assertThat(isLate).isFalse();
     }
 
     @Test
     public void should_be_late_when_check_in_date_is_thursday_night() throws Exception {
-        regisCheckIn.setCheckingDate(LocalDateTime.of(2017, Month.OCTOBER,27,21,1,0,1));
+        regisCheckIn.setCheckingDate(LocalDateTime.of(2017, Month.OCTOBER, 27, 21, 1, 0, 1));
         Boolean isLate = regisCheckIn.isLate();
         Assertions.assertThat(isLate).isTrue();
+    }
+
+    @Test
+    public void should_not_be_late_when_check_in_date_is_before_the_begin_of_cold_meals() throws Exception {
+        regisCheckIn.setCheckingDate(LocalDateTime.of(2017, Month.OCTOBER, 27, 20, 1, 0, 1));
+        Boolean isLate = regisCheckIn.isLate();
+        Assertions.assertThat(isLate).isFalse();
     }
 }

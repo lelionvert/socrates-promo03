89637d5c869b42ca6652511a489fbcf9b85ce513
YeahMama <wallon.xu@lcdlv.fr>
2017-10-17 15:39:38 +0200

    [US3]: Refactoring count method

--- java/src/main/java/com/lacombe/promo3/ColdMealsCounter.java
+++ java/src/main/java/com/lacombe/promo3/ColdMealsCounter.java
@@ -3,6 +3,7 @@ package com.lacombe.promo3;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 class ColdMealsCounter {
     static final LocalDateTime BEGIN_COLD_MEALS_DATE = LocalDateTime.of(2017, Month.OCTOBER, 27, 21, 0, 0, 0);
@@ -17,14 +18,7 @@ class ColdMealsCounter {
 
     Integer count() {
         Collection<CheckIn> checkIns = checkInProvider.getCheckIns();
-
-        int nbColdMeals = 0;
-        for (CheckIn checkIn : checkIns) {
-            if (checkIn.isBetween(BEGIN_COLD_MEALS_DATE, END_COLD_MEALS_DATE)) {
-                nbColdMeals += 1;
-            }
-        }
-
-        return nbColdMeals;
+        checkIns = checkIns.stream().filter(checkIn -> checkIn.isBetween(BEGIN_COLD_MEALS_DATE, END_COLD_MEALS_DATE)).collect(Collectors.toList());
+        return checkIns.size();
     }
 }

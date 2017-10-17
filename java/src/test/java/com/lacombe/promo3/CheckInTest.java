4ab199a8d1d556049f7f5a7e9305e5090271423c
YeahMama <wallon.xu@lcdlv.fr>
2017-10-17 15:21:27 +0200

    [US3]: Add boundary tests on Checkin

--- java/src/test/java/com/lacombe/promo3/CheckInTest.java
+++ java/src/test/java/com/lacombe/promo3/CheckInTest.java
@@ -39,6 +39,20 @@ public class CheckInTest {
     }
 
     @Test
+    public void should_not_be_between_when_check_in_date_is_exactly_the_given_begin_date() throws Exception {
+        regisCheckIn.setCheckInDate(ColdMealsCounter.BEGIN_COLD_MEALS_DATE);
+        Boolean checkInBetween = regisCheckIn.isBetween(ColdMealsCounter.BEGIN_COLD_MEALS_DATE, ColdMealsCounter.END_COLD_MEALS_DATE);
+        assertThat(checkInBetween).isFalse();
+    }
+
+    @Test
+    public void should_not_be_between_when_check_in_date_is_exactly_the_given_end_date() throws Exception {
+        regisCheckIn.setCheckInDate(ColdMealsCounter.END_COLD_MEALS_DATE);
+        Boolean checkInBetween = regisCheckIn.isBetween(ColdMealsCounter.BEGIN_COLD_MEALS_DATE, ColdMealsCounter.END_COLD_MEALS_DATE);
+        assertThat(checkInBetween).isFalse();
+    }
+
+    @Test
     public void should_be_between_when_check_in_date_is_between_the_given_dates() throws Exception {
         regisCheckIn.setCheckInDate(LocalDateTime.of(2017, Month.OCTOBER, 27, 21, 1, 0, 1));
         Boolean checkInBetween = regisCheckIn.isBetween(ColdMealsCounter.BEGIN_COLD_MEALS_DATE, ColdMealsCounter.END_COLD_MEALS_DATE);

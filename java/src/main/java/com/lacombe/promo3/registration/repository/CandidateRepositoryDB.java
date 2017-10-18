75a3228eb54ec61994bcea31a83bd7174f758011
MelodyBibi <melody.scholtes@gmail.com>
2017-10-18 11:11:09 +0200

    API REST : add find all method

--- java/src/main/java/com/lacombe/promo3/registration/repository/CandidateRepositoryDB.java
+++ java/src/main/java/com/lacombe/promo3/registration/repository/CandidateRepositoryDB.java
@@ -1,20 +1,60 @@
 package com.lacombe.promo3.registration.repository;
 
 import com.lacombe.promo3.registration.model.Candidate;
+import com.lacombe.promo3.registration.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @Repository
-public class CandidateRepositoryDB {
+public class CandidateRepositoryDB implements CandidateRepository {
 
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
     public void setDataSource(DataSource dataSource){
         jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Collection<Candidate> findAll(){
+        Collection<Candidate> candidates = new ArrayList<>();
+
+        final String query = "SELECT * FROM candidate limit 10";
+        List<Map<String, Object>> results = jdbcTemplate.queryForList(query);
+        results.forEach( row -> {
+            Candidate candidate = new Candidate(
+                    Email.of((String) row.get("email")));
+                    candidates.add(candidate);
+                    System.out.println(candidate);
+                }
+        );
+
+        return candidates;
+    }
+
+    @Override
+    public void add(Candidate candidate) {
+
+    }
+
+    @Override
+    public boolean hasAlready(Candidate candidate) {
+        return false;
+    }
+
+    @Override
+    public Collection<Email> getEmails() {
+        return null;
+    }
+
+    @Override
+    public Candidate getByEmail(String email) {
+        return null;
     }
 }

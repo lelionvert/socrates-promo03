0beca7d1985e3b96f6bbb55e8efd63c2551df3ea
Cyrille NANTCHOUANG <cyrille.nantchouang@gmail.com>
2017-10-20 11:00:59 +0200

    Unit test for include method

--- java/src/main/java/com/lacombe/promo3/Period.java
+++ java/src/main/java/com/lacombe/promo3/Period.java
@@ -6,10 +6,17 @@ import java.time.LocalDateTime;
 
 public class Period {
 
+    private final LocalDateTime begin;
+    private final LocalDateTime end;
+
     public Period(LocalDateTime begin, LocalDateTime end) {
+        this.begin = begin;
+        this.end = end;
     }
 
     public Boolean include(LocalDateTime dateTime) {
-        return true;
+        return dateTime != null
+                && dateTime.isAfter(begin)
+                && dateTime.isBefore(end);
     }
 }
--- java/src/test/java/com/lacombe/promo3/meals/PeriodTest.java
+++ java/src/test/java/com/lacombe/promo3/meals/PeriodTest.java
@@ -22,4 +22,19 @@ public class PeriodTest {
         //Assert
         Assertions.assertThat(isIncluded).isTrue();
     }
+
+    @Test
+    public void should_not_be_included_in_the_period() throws Exception {
+        //Arrange
+        LocalDateTime begin = LocalDateTime.of(2017, 10, 20, 21, 30);
+        LocalDateTime end = begin.plusDays(1);
+        Period period = new Period(begin, end);
+
+        //Act
+        LocalDateTime dateTime = begin.minusHours(1);
+        Boolean isIncluded = period.include(dateTime);
+
+        //Assert
+        Assertions.assertThat(isIncluded).isFalse();
+    }
 }

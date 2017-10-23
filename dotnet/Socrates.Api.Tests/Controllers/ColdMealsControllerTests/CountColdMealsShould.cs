5d7fd976949ba5f1694a6d5a624ba5682d1de0de
ismailnguyen <nguyen.ismail@gmail.com>
2017-10-23 09:10:29 +0200

    Add empty web and api projects

---
+++ dotnet/Socrates.Api.Tests/Controllers/ColdMealsControllerTests/CountColdMealsShould.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Socrates.Api.Tests.Controllers.ColdMealsControllerTests
+{
+    class CountColdMealsShould
+    {
+        [Test]
+        public void Return_Status_Code_OK()
+        {
+
+        }
+    }
+}
